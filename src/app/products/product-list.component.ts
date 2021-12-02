322ba75874a5e43d3adb8bd9a4ba70882f10c47d
fgregor <filip.gregor@gmail.com>
2021-12-02 22:06:44 +0100

    implemented a getter and setter for _listFilter backing field

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -11,7 +11,16 @@ export class ProductListComponent implements OnInit{
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
+
+    private _listFilter: string = '';
+    get listFilter(): string{
+      return this._listFilter;
+    }
+    set listFilter(value: string){
+      this._listFilter = value;
+      console.log('In setter:', value);
+    }
+
     products: IProduct[] =
     [
         {
@@ -71,6 +80,6 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit(): void {
-      console.log('OnInit method.');
+      this._listFilter = 'cart';
     }
 }
